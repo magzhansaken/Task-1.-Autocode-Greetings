da266238783c424e9d085c7b61aca41ec0dbee53
Task #1. Autocode Greetings <magzhansaken@mail.ru>
2022-12-15 11:52:51 +0600

    initial commit

---
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -0,0 +1,8 @@
+package com.mjc.stage0;
+
+public class FirstApplication {
+    public static void main(String[] args) {
+        // Write your code here!
+        System.out.println("Hello, World!");
+    }
+}
